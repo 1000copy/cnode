dbb012c13f99a2bede3ecddfeabc249c88a1437a
1000copy <1000copy@gmail.com>
2017-08-14 15:35:21 +0800

    page 2

--- cnode/AppDelegate.swift
+++ cnode/AppDelegate.swift
@@ -1,7 +1,5 @@
 import UIKit
-import Alamofire
-import AlamofireObjectMapper
-import ObjectMapper
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
@@ -20,57 +18,15 @@ class Nav: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
-        self.pushViewController(Level1(), animated: true)
+        self.pushViewController(TopicsPage(), animated: true)
     }
 }
-class Level1 : UITableViewController{
-    var arr : Topics?
-    let MyIdentifier = "cell"
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        Bar.foo(){
-            self.arr = $0
-            print(self.arr?.data?[0].top)
-            self.tableView.reloadData()
-        }
-        tableView.register(Cell.self, forCellReuseIdentifier: MyIdentifier)
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if arr != nil {
-            return (arr?.data!.count)!
-        }else{
-            return 0
-        }
-    }
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let a = tableView.dequeueReusableCell(withIdentifier: MyIdentifier) as! Cell
-        let topic = arr?.data![indexPath.row]
-        a._title.text = topic?.title
-        
-        a._top.isHidden = !(topic?.top)!
-        a._author.text = topic?.author?.loginname
-        a._hot.text = "\((topic?.reply_count)!)/\((topic?.visit_count)!)"
-        let d = date((topic?.create_at)!)!
-        a._created.text = "创建于：\(d.getElapsedInterval())"
-        let e = date((topic?.last_reply_at)!)!
-        a._lastReplied.text = "\(e.getElapsedInterval())"
-        a._avatar.kf.setImage(with:URL(string:(topic?.author?.avatar_url)!))
-        return a
-    }
-    func date(_ str : String)-> Date?{
-        print(str)
-        let dateformatter = DateFormatter()
-        dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
-        return dateformatter.date(from: str)
-    }
-    
+func date(_ str : String)-> Date?{
+    print(str)
+    let dateformatter = DateFormatter()
+    dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    return dateformatter.date(from: str)
 }
-import Kingfisher
-
-//Swift convert time to time ago
 extension Date {
     func getElapsedInterval() -> String {
         
@@ -120,134 +76,5 @@ extension UIImage {
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image!
-    }
-}
-import Cartography
-class Cell : UITableViewCell{
-    var _title = UILabel()
-    var _top = UILabel()
-    var _avatar = UIImageView()
-    var _author = UILabel()
-    var _hot = UILabel()
-    var _created = UILabel()
-    var _lastReplied = UILabel()
-    override func layoutSubviews() {
-        self.contentView.addSubview(_title)
-        self.contentView.addSubview(_avatar)
-//        _avatar.image = UIImage.imageWithColor(.blue)
-        _top.text = "置顶"
-        _top.backgroundColor = .green
-        self.contentView.addSubview(_top)
-        self.contentView.addSubview(_author)
-        self.contentView.addSubview(_hot)
-        self.contentView.addSubview(_created)
-        self.contentView.addSubview(_lastReplied)
-        _hot.textAlignment = .right
-        _lastReplied.textAlignment = .right
-//        _lastReplied.isHidden = true
-        constrain(contentView,_title,_avatar,_top){
-            $1.left == $2.right  + 20
-            $1.top  == $0.top + 5
-            $1.width  == 300
-            $1.height  == 20
-            
-            $2.left == $0.left  + 5
-            $2.top  == $3.bottom + 5
-            $2.width  == 38
-            $2.height  == 38
-            
-            $3.left == $0.left  + 5
-            $3.top  == $0.top + 5
-            $3.width  == 40
-            $3.height  == 20
-            
-        }
-        constrain(contentView,_avatar,_author){
-            $2.left == $1.right  + 5
-            $2.top  == $1.top
-            $2.width  == 150
-            $2.height  == 20
-        }
-        constrain(contentView,_avatar,_hot){
-            $2.right == $0.right  -  5
-            $2.top  == $1.top
-            $2.width  == 200
-            $2.height  == 20
-        }
-        constrain(contentView,_avatar,_created,_lastReplied){
-            $2.left == $1.right  +  5
-            $2.bottom  == $1.bottom
-            $2.width  == 200
-            $2.height  == 20
-            
-            $3.right == $0.right  -  5
-            $3.bottom  == $1.bottom
-            $3.width  == 200
-            $3.height  == 20
-        }
-    }
-}
-class Bar{
-    class func foo(done:@escaping (_ t : Topics)->Void){
-        let URL = "https://cnodejs.org/api/v1/topics?page=1"
-        Alamofire.request(URL).responseObject { (response: DataResponse<Topics>) in
-            let topics = response.result.value
-            done(topics!)
-        }
-    }
-}
-class Topics: Mappable {
-    var success: Bool?
-    var data : [Topic]?
-    required init?(map: Map){
-        
-    }
-    
-    func mapping(map: Map) {
-        success <- map["success"]
-        data <- map["data"]
-    }
-}
-//content title last_reply_at good top reply_count visit_count create_at author{loginname,avatar_url}
-class Author : Mappable{
-    var loginname: String?
-    var avatar_url: String?
-    required init?(map: Map){
-        
-    }
-    func mapping(map: Map) {
-        loginname <- map["loginname"]
-        avatar_url <- map["avatar_url"]
-    }
-}
-class Topic: Mappable {
-    var id: String?
-    var author_id : String?
-    var tab : String?
-    var content : String?
-    var title : String?
-    var last_reply_at : String?
-    var good : String?
-    var top : Bool?
-    var reply_count : Int?
-    var visit_count : Int?
-    var create_at : String?
-    var author : Author?
-    required init?(map: Map){
-        
-    }
-    func mapping(map: Map) {
-        id <- map["id"]
-        author_id <- map["author_id"]
-        content <- map["content"]
-        title <- map["title"]
-        last_reply_at <- map["last_reply_at"]
-        good <- map["good"]
-        top <- map["top"]
-        reply_count <- map["reply_count"]
-        visit_count <- map["visit_count"]
-        create_at <- map["create_at"]
-        author <- map["author"]
-        tab <- map["tab"]
     }
 }
---
+++ cnode/topicPage.swift
@@ -0,0 +1,252 @@
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
+import UIKit
+import Kingfisher
+class TopicPage : UITableViewController{
+    fileprivate var result : Result?
+    override func viewDidLoad() {
+        Bar.foo{
+            self.result = $0
+            print(self.result?.success)
+            self.tableView.reloadData()
+        }
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if result == nil {
+            return 1
+        }else{
+            return (result?.data?.replies?.count)! + 1
+        }
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let url = URL(string:"https://")
+        if result != nil {
+            if indexPath.row == 0 {
+                let a = Cell()
+                a._title.text = result?.data?.title
+                a._avatar.kf.setImage(with:URL(string:(result?.data?.author?.avatar_url)!))
+                let topic = result?.data
+                a._top.isHidden = !(topic?.top)!
+                a._author.text = topic?.author?.loginname
+                a._hot.text = "\((topic?.reply_count)!)/\((topic?.visit_count)!)"
+                let d = date((topic?.create_at)!)!
+                a._created.text = "创建于：\(d.getElapsedInterval())"
+                let e = date((topic?.last_reply_at)!)!
+                a._lastReplied.text = "\(e.getElapsedInterval())"
+                a._avatar.kf.setImage(with:URL(string:(topic?.author?.avatar_url)!))
+                a._content.loadHTMLString((topic?.content)!, baseURL: url)
+                return a
+            }else{
+                let a = ReplyCell()
+                let r = result?.data?.replies?[indexPath.row - 1]
+                a._avatar.kf.setImage(with:URL(string:(r?.author?.avatar_url)!))
+                let html = r?.content
+                a._content.loadHTMLString((html)!, baseURL: url)
+                a._created.text = date((r?.created)!)?.getElapsedInterval()
+                a._author.text = r?.author?.loginname
+                return a
+            }
+        }
+        return UITableViewCell()
+    }
+    
+}
+import Kingfisher
+import Cartography
+fileprivate class ReplyCell : UITableViewCell,UIWebViewDelegate{
+    var _content = UIWebView()
+    var _author  = UILabel()
+    var _created = UILabel()
+    var _avatar  = UIImageView()
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+//        let str = webView.stringByEvaluatingJavaScript(from: "document.body.scrollHeight;")
+//        let height = Int(str!)
+//        print(height)
+////        webView.frame.size.height = 1
+////        webView.frame.size = webView.sizeThatFits(CGSize.zero)
+////        print(webView.frame.size)
+//        let h = webView.scrollView.contentSize.height
+//        print(h)
+//        print(webView.frame.height)
+        webView.frame.size.height = 1
+        webView.frame.size = webView.sizeThatFits(.zero)
+    }
+    fileprivate override func layoutSubviews() {
+         self.contentView.addSubview(_content)
+        self.contentView.addSubview(_author)
+        self.contentView.addSubview(_created)
+        self.contentView.addSubview(_avatar)
+        _created.textAlignment = .right
+        _content.isUserInteractionEnabled = false
+        _content.delegate = self
+        constrain(contentView,_avatar,_created,_content,_author){
+            $1.left == $0.left + 5
+            $1.top == $0.top + 5
+            $1.height == 38
+            $1.width == 38
+            
+            $4.left == $1.right + 5
+            $4.top == $1.top
+            
+            $3.top == $1.bottom + 5
+            $3.left == $1.right + 5
+            $3.right == $0.right
+            $3.bottom == $0.bottom
+            
+            $2.top == $1.top
+            $2.right == $0.right - 5
+        }
+    }
+}
+fileprivate class Cell : UITableViewCell{
+    var _title = UILabel()
+    var _top = UILabel()
+    var _avatar = UIImageView()
+    var _author = UILabel()
+    var _hot = UILabel()
+    var _created = UILabel()
+    var _lastReplied = UILabel()
+    var _content = UIWebView()
+    override func layoutSubviews() {
+        self.contentView.addSubview(_title)
+        self.contentView.addSubview(_avatar)
+        //        _avatar.image = UIImage.imageWithColor(.blue)
+        _top.text = "置顶"
+        _top.backgroundColor = .green
+        self.contentView.addSubview(_top)
+        self.contentView.addSubview(_author)
+        self.contentView.addSubview(_hot)
+        self.contentView.addSubview(_created)
+        self.contentView.addSubview(_lastReplied)
+        self.contentView.addSubview(_content)
+        _hot.textAlignment = .right
+        _lastReplied.textAlignment = .right
+        //        _lastReplied.isHidden = true
+        constrain(contentView,_title,_avatar,_top){
+            $1.left == $2.right  + 20
+            $1.top  == $0.top + 5
+            $1.width  == 300
+            $1.height  == 20
+            
+            $2.left == $0.left  + 5
+            $2.top  == $3.bottom + 5
+            $2.width  == 38
+            $2.height  == 38
+            
+            $3.left == $0.left  + 5
+            $3.top  == $0.top + 5
+            $3.width  == 40
+            $3.height  == 20
+            
+        }
+        constrain(contentView,_avatar,_author){
+            $2.left == $1.right  + 5
+            $2.top  == $1.top
+            $2.width  == 150
+            $2.height  == 20
+        }
+        constrain(contentView,_avatar,_hot){
+            $2.right == $0.right  -  5
+            $2.top  == $1.top
+            $2.width  == 200
+            $2.height  == 20
+        }
+        constrain(contentView,_avatar,_created,_lastReplied){
+            $2.left == $1.right  +  5
+            $2.bottom  == $1.bottom
+            $2.width  == 200
+            $2.height  == 20
+            
+            $3.right == $0.right  -  5
+            $3.bottom  == $1.bottom
+            $3.width  == 200
+            $3.height  == 20
+        }
+        constrain(contentView,_avatar,_content){
+            $2.left == $1.left
+            $2.top  == $1.bottom + 5
+            $2.width  == $0.width
+            $2.bottom  == $0.bottom
+        }
+    }
+}
+fileprivate class Bar{
+    class func foo(done:@escaping (_ t : Result)->Void){
+        let id = "598f28a8e104026c52101860"
+        let URL = "https://cnodejs.org/api/v1/topic/\(id)"
+        Alamofire.request(URL).responseObject { (response: DataResponse<Result>) in
+            let topics = response.result.value
+            done(topics!)
+        }
+    }
+}
+fileprivate class Result: Mappable {
+    var success: Bool?
+    var data : Data?
+    required init?(map: Map){
+        
+    }
+    
+    func mapping(map: Map) {
+        success <- map["success"]
+        data <- map["data"]
+    }
+}
+//content title last_reply_at good top reply_count visit_count create_at author{loginname,avatar_url}
+fileprivate class Data : Mappable{
+    var content : String?
+    var title : String?
+    var replies : [Reply]?
+    var author : Author?
+    var author_id : String?
+    var tab : String?
+    var last_reply_at : String?
+    var good : String?
+    var top : Bool?
+    var reply_count : Int?
+    var visit_count : Int?
+    var create_at : String?
+    required init?(map: Map){
+        
+    }
+    func mapping(map: Map) {
+        author <- map["author"]
+        content <- map["content"]
+        title <- map["title"]
+        author_id <- map["author_id"]
+
+        last_reply_at <- map["last_reply_at"]
+        good <- map["good"]
+        top <- map["top"]
+        reply_count <- map["reply_count"]
+        visit_count <- map["visit_count"]
+        create_at <- map["create_at"]
+        //        author <- map["author"]
+        tab <- map["tab"]
+        replies <- map["replies"]
+    }
+}
+
+fileprivate class Reply: Mappable {
+    var id: String?
+    var content : String?
+    var title : String?
+    var author : Author?
+    var created : String?
+    required init?(map: Map){
+        
+    }
+    func mapping(map: Map) {
+        id <- map["id"]
+        content <- map["content"]
+        title <- map["title"]
+        author <- map["author"]
+        created <- map["create_at"]
+    }
+}
